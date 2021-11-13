9db2b9ab4b3e7af2c849b0dcc90939ad420f4fdc
Mike Eng <mike.eng@adhocteam.us>
2021-11-12 21:47:44 -0500

    custom taxonomies for year and ensemble

--- content/mu-plugins/custom-taxonomies.php
+++ content/mu-plugins/custom-taxonomies.php
@@ -31,5 +31,30 @@ function register_taxonomy_ensemble() {
      );
      register_taxonomy( 'ensemble', array( 'page','attachment', 'year-story' ), $args );
 }
+function register_taxonomy_vhs_year() {
+     $labels = array(
+         'name'              => _x( 'Years', 'taxonomy general name' ),
+         'singular_name'     => _x( 'Year', 'taxonomy singular name' ),
+         'search_items'      => __( 'Search Years' ),
+         'all_items'         => __( 'All Years' ),
+         'parent_item'       => __( 'Parent Year' ),
+         'parent_item_colon' => __( 'Parent Year:' ),
+         'edit_item'         => __( 'Edit Year' ),
+         'update_item'       => __( 'Update Year' ),
+         'add_new_item'      => __( 'Add New Year' ),
+         'new_item_name'     => __( 'New Year Name' ),
+         'menu_name'         => __( 'Year' ),
+     );
+     $args   = array(
+         'hierarchical'      => false, // make it hierarchical (like categories)
+         'labels'            => $labels,
+         'show_ui'           => true,
+         'show_admin_column' => true,
+         'query_var'         => true,
+         'rewrite'           => [ 'slug' => 'vhs_year' ],
+     );
+     register_taxonomy( 'vhs_year', array( 'page','attachment', 'year-story' ), $args );
+}
 add_action( 'init', 'register_taxonomy_ensemble' );
+add_action( 'init', 'register_taxonomy_vhs_year' );
 ?>
