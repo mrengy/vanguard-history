e70b257791984a46a8435f21ea53776080f3b4c9
Mike Eng <mike.eng@adhocteam.us>
2021-11-14 18:02:24 -0500

    initial wp gulp configuration and restructuring

--- content/themes/vanguard-history/functions.php
+++ content/themes/vanguard-history/functions.php
@@ -143,7 +143,7 @@ function vanguard_history_scripts() {
 	wp_enqueue_style( 'vanguard-history-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'vanguard-history-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'vanguard-history-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'vanguard-history-navigation', get_template_directory_uri() . '/assets/js/custom.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
