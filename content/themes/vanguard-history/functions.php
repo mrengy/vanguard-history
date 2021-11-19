b70dfd4165479d8e23d20e946e936ca7ec942ef6
Mike Eng <mike.eng@adhocteam.us>
2021-11-18 21:53:59 -0500

    corrected js path, added test js

--- content/themes/vanguard-history/functions.php
+++ content/themes/vanguard-history/functions.php
@@ -140,9 +140,11 @@ add_action( 'widgets_init', 'vanguard_history_widgets_init' );
  * Enqueue scripts and styles.
  */
 function vanguard_history_scripts() {
-	wp_enqueue_style( 'vanguard-history-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'vanguard-history-style', get_stylesheet_uri(), array(), false );
 
-	wp_enqueue_script( 'vanguard-history-navigation', get_template_directory_uri() . '/assets/js/custom/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'vanguard-history-navigation', get_template_directory_uri() . '/js/custom/navigation.js', array(), false, true );
+
+	wp_enqueue_script( 'vanguard-history-test', get_template_directory_uri() . '/js/custom/test.js', array('jquery'), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
