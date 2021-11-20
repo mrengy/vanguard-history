b0998a7cdb1c927653d1305923b7f4ca0f5fbffd
Mike Eng <mike.eng@adhocteam.us>
2021-11-19 21:58:03 -0500

    exclusive custom taxonomy for media visibility per https://www.toptal.com/wordpress/wordpress-taxonomy-tutorial but saving not working

--- content/themes/vanguard-history/functions.php
+++ content/themes/vanguard-history/functions.php
@@ -183,55 +183,111 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Custom taxonomies
  */
 function register_taxonomy_ensemble() {
-     $labels = array(
-         'name'              => _x( 'Ensemble', 'taxonomy general name' ),
-         'singular_name'     => _x( 'Ensemble', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Ensembles' ),
-         'all_items'         => __( 'All Ensembles' ),
-         'parent_item'       => __( 'Parent Ensemble' ),
-         'parent_item_colon' => __( 'Parent Ensemble:' ),
-         'edit_item'         => __( 'Edit Ensemble' ),
-         'update_item'       => __( 'Update Ensemble' ),
-         'add_new_item'      => __( 'Add New Ensemble' ),
-         'new_item_name'     => __( 'New Ensemble Name' ),
-         'menu_name'         => __( 'Ensemble' ),
-     );
-     $args   = array(
-         'hierarchical'      => false, // make it hierarchical (like categories)
-         'labels'            => $labels,
-         'show_ui'           => true,
-         'show_admin_column' => true,
-         'query_var'         => true,
-         'rewrite'           => [ 'slug' => 'ensemble' ],
-     );
-     register_taxonomy( 'ensemble', array( 'page','attachment', 'year_story' ), $args );
+   $labels = array(
+       'name'              => _x( 'Ensemble', 'taxonomy general name' ),
+       'singular_name'     => _x( 'Ensemble', 'taxonomy singular name' ),
+       'search_items'      => __( 'Search Ensembles' ),
+       'all_items'         => __( 'All Ensembles' ),
+       'parent_item'       => __( 'Parent Ensemble' ),
+       'parent_item_colon' => __( 'Parent Ensemble:' ),
+       'edit_item'         => __( 'Edit Ensemble' ),
+       'update_item'       => __( 'Update Ensemble' ),
+       'add_new_item'      => __( 'Add New Ensemble' ),
+       'new_item_name'     => __( 'New Ensemble Name' ),
+       'menu_name'         => __( 'Ensemble' ),
+   );
+   $args   = array(
+       'hierarchical'      => false, // make it hierarchical (like categories)
+       'labels'            => $labels,
+       'show_ui'           => true,
+       'show_admin_column' => true,
+       'query_var'         => true,
+       'rewrite'           => [ 'slug' => 'ensemble' ],
+   );
+   register_taxonomy( 'ensemble', array( 'page','attachment', 'year_story' ), $args );
 }
 function register_taxonomy_vhs_year() {
-     $labels = array(
-         'name'              => _x( 'Year', 'taxonomy general name' ),
-         'singular_name'     => _x( 'Year', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Years' ),
-         'all_items'         => __( 'All Years' ),
-         'parent_item'       => __( 'Parent Year' ),
-         'parent_item_colon' => __( 'Parent Year:' ),
-         'edit_item'         => __( 'Edit Year' ),
-         'update_item'       => __( 'Update Year' ),
-         'add_new_item'      => __( 'Add New Year' ),
-         'new_item_name'     => __( 'New Year Name' ),
-         'menu_name'         => __( 'Year' ),
-     );
-     $args   = array(
-         'hierarchical'      => false, // make it hierarchical (like categories)
-         'labels'            => $labels,
-         'show_ui'           => true,
-         'show_admin_column' => true,
-         'query_var'         => true,
-         'rewrite'           => [ 'slug' => 'vhs_year' ],
-     );
-     register_taxonomy( 'vhs_year', array( 'page','attachment', 'year_story' ), $args );
+   $labels = array(
+       'name'              => _x( 'Year', 'taxonomy general name' ),
+       'singular_name'     => _x( 'Year', 'taxonomy singular name' ),
+       'search_items'      => __( 'Search Years' ),
+       'all_items'         => __( 'All Years' ),
+       'parent_item'       => __( 'Parent Year' ),
+       'parent_item_colon' => __( 'Parent Year:' ),
+       'edit_item'         => __( 'Edit Year' ),
+       'update_item'       => __( 'Update Year' ),
+       'add_new_item'      => __( 'Add New Year' ),
+       'new_item_name'     => __( 'New Year Name' ),
+       'menu_name'         => __( 'Year' ),
+   );
+   $args   = array(
+       'hierarchical'      => false, // make it hierarchical (like categories)
+       'labels'            => $labels,
+       'show_ui'           => true,
+       'show_admin_column' => true,
+       'query_var'         => true,
+       'rewrite'           => [ 'slug' => 'vhs_year' ],
+   );
+   register_taxonomy( 'vhs_year', array( 'page','attachment', 'year_story' ), $args );
 }
+function register_taxonomy_media_visibility(){
+	$labels = array(
+			'name'              => _x( 'Visibility Statuses', 'taxonomy general name' ),
+			'singular_name'     => _x( 'Visibility Status', 'taxonomy singular name' ),
+			'search_items'      => __( 'Search Visibility Statuses' ),
+			'all_items'         => __( 'All Visitbility Statuses' ),
+			'parent_item'       => __( 'Parent Visibility Status' ),
+			'parent_item_colon' => __( 'Parent Visibility Status:' ),
+			'edit_item'         => __( 'Edit Visibility Status' ),
+			'update_item'       => __( 'Update Visibility Status' ),
+			'add_new_item'      => __( 'Add New Visibility Status' ),
+			'new_item_name'     => __( 'New Visibility Status Name' ),
+			'menu_name'         => __( 'Visibility Status' ),
+	);
+	$args   = array(
+			'hierarchical'      => false, // make it hierarchical (like categories)
+			'labels'            => $labels,
+			'show_in_quick_edit' => false,
+			'meta_box_cb' => false,
+	);
+	register_taxonomy( 'media_visibility', 'attachment', $args );
+}
+
+function add_custom_meta_box_media_visibility(){
+	add_meta_box( 'taxonomy_box', __('Media Visibility Status'), 'fill_custom_meta_box_content', 'attachment' ,'side');
+}
+
+// set closed set of options for media Visibility
+function fill_custom_meta_box_content( $post ) {
+	$terms = get_terms( array(
+		'taxonomy' => 'media_visibility',
+		'hide_empty' => false // Retrieve all terms
+	));
+
+	// We assume that there is a single category
+	$currentTaxonomyValue = get_the_terms($post->ID, 'media_visibility')[0];
+?>
+	<p>Choose taxonomy value</p>
+	<p>
+		<?php foreach($terms as $term): ?>
+			<input type="radio" name="media_visibility" id="taxonomy_term_<?php echo $term->term_id;?>" value="<?php echo $term->term_id;?>"<?php if($term->term_id==$currentTaxonomyValue->term_id) echo "checked"; ?>>
+			<label for="taxonomy_term_<?php echo $term->term_id;?>"><?php echo $term->name; ?></label>
+			</input><br/>
+		<?php endforeach; ?>
+	</p>
+<?php
+}
+
+function save_media_visibility($post_id){
+	if ( isset( $_REQUEST['media_visibility'] ) )
+		wp_set_object_terms($post_id, (int)sanitize_text_field( $_POST['media_visibility'] ), 'media_visibility');
+}
+
 add_action( 'init', 'register_taxonomy_ensemble' );
 add_action( 'init', 'register_taxonomy_vhs_year' );
+add_action( 'init', 'register_taxonomy_media_visibility' );
+add_action('add_meta_boxes', 'add_custom_meta_box_media_visibility');
+add_action('save_post', 'save_media_visibility');
 
 /**
  * Custom post types
