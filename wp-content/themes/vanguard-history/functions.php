c426c8f289429ecbcd94f34c36689d89f9aec334
Mike Eng <mike.eng@adhocteam.us>
2022-04-11 22:07:44 -0400

    logging info from uploaded file

--- wp-content/themes/vanguard-history/functions.php
+++ wp-content/themes/vanguard-history/functions.php
@@ -331,9 +331,59 @@ add_action('init', 'vhs_custom_post_type');
  * Upload form submissions to media library
  */
 function form_to_media_library($entry){
+	// WordPress environment
+  /*
+	require( dirname(__FILE__) . '/../../../wp-load.php' );
+
+	$wordpress_upload_dir = wp_upload_dir();
+	// $wordpress_upload_dir['path'] is the full server path to wp-content/uploads/2017/05, for multisite works good as well
+	// $wordpress_upload_dir['url'] the absolute URL to the same folder, actually we do not need it, just to show the link to file
+	$i = 1; // number of tries when the file with the same name is already exists
+
+	$vhs_uploaded_file = $entry[1];
+	$new_file_path = $wordpress_upload_dir['path'] . '/' . $vhs_uploaded_file['name'];
+	$new_file_mime = mime_content_type( $vhs_uploaded_file['tmp_name'] );
+
+	if( $vhs_uploaded_file['size'] > wp_max_upload_size() )
+		die( 'It is too large than expected.' );
+
+	if( !in_array( $new_file_mime, get_allowed_mime_types() ) )
+		die( 'WordPress doesn\'t allow this type of uploads.' );
+
+	while( file_exists( $new_file_path ) ) {
+		$i++;
+		$new_file_path = $wordpress_upload_dir['path'] . '/' . $i . '_' . $vhs_uploaded_file['name'];
+	}
+
+	// looks like everything is OK
+	if( move_uploaded_file( $vhs_uploaded_file['tmp_name'], $new_file_path ) ) {
 
 
-	do_action( 'qm/debug', $entry );
+		$upload_id = wp_insert_attachment( array(
+			'guid'           => $new_file_path,
+			'post_mime_type' => $new_file_mime,
+			'post_title'     => preg_replace( '/\.[^.]+$/', '', $vhs_uploaded_file['name'] ),
+			'post_content'   => '',
+			'post_status'    => 'inherit'
+		), $new_file_path );
+
+		// wp_generate_attachment_metadata() won't work if you do not include this file
+		require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+		// Generate and save the attachment metas into the database
+		wp_update_attachment_metadata( $upload_id, wp_generate_attachment_metadata( $upload_id, $new_file_path ) );
+
+	}
+	*/
+
+	$upload_path = GFFormsModel::get_upload_path( $entry[ 'form_id' ] );
+  $upload_url = GFFormsModel::get_upload_url( $entry[ 'form_id' ] );
+
+  $filename = str_replace( $upload_url, $upload_path, $entry[ '1' ] );
+
+	do_action( 'qm/debug', $filename );
+	do_action( 'qm/debug', $upload_path );
+	do_action( 'qm/debug', $upload_url );
 }
 
 add_action( 'gform_after_submission_1', 'form_to_media_library', 10, 2 );
