7dfdde531f3042b810f8b32ff3e80c0bd9167ade
Mike Eng <mike.eng@adhocteam.us>
2022-04-19 14:40:36 -0400

    removing stale comments

--- wp-content/themes/vanguard-history/functions.php
+++ wp-content/themes/vanguard-history/functions.php
@@ -331,41 +331,6 @@ add_action('init', 'vhs_custom_post_type');
  * Upload form submissions to media library
  */
 function form_to_media_library($entry){
-	// WordPress environment
-  /*
-	// $filename should be the path to a file in the upload directory.
-$filename = '/path/to/uploads/2013/03/filename.jpg';
-
-// The ID of the post this attachment is for.
-$parent_post_id = 37;
-
-// Check the type of file. We'll use this as the 'post_mime_type'.
-$filetype = wp_check_filetype( basename( $filename ), null );
-
-// Get the path to the upload directory.
-$wp_upload_dir = wp_upload_dir();
-
-// Prepare an array of post data for the attachment.
-$attachment = array(
-    'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
-    'post_mime_type' => $filetype['type'],
-    'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
-    'post_content'   => '',
-    'post_status'    => 'inherit'
-);
-
-// Insert the attachment.
-$attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
-
-// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-require_once( ABSPATH . 'wp-admin/includes/image.php' );
-
-// Generate the metadata for the attachment, and update the database record.
-$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-wp_update_attachment_metadata( $attach_id, $attach_data );
-
-set_post_thumbnail( $parent_post_id, $attach_id );
-	*/
 	// from https://developer.wordpress.org/reference/functions/wp_insert_attachment/#div-comment-948
 
 	// set filename
@@ -373,7 +338,7 @@ set_post_thumbnail( $parent_post_id, $attach_id );
   $upload_url = GFFormsModel::get_upload_url( $entry[ 'form_id' ] );
   $filename_verbose = str_replace( $upload_url, $upload_path, $entry[ '1' ] );
 	$filename = trim($filename_verbose, ' "[]\ ');
-	do_action( 'qm/debug', $filename );
+
 
 	// check the type of file. We'll use this as the 'post_mime_type'
 	$filetype = wp_check_filetype( basename( $filename ), null );
@@ -399,6 +364,8 @@ set_post_thumbnail( $parent_post_id, $attach_id );
 	// Generate the metadata for the attachment, and update the database record.
 	$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
 	wp_update_attachment_metadata( $attach_id, $attach_data );
+
+	do_action( 'qm/debug', $attach_id );
 }
 
 // targets the specific form by form ID of 1
