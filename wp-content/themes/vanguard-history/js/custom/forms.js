f4f1ba88b683fa4e2eb181a9187664c0fe49909a
Mike Eng <mike.eng@adhocteam.us>
2022-07-12 12:38:02 -0400

    message for upload error

--- wp-content/themes/vanguard-history/js/custom/forms.js
+++ wp-content/themes/vanguard-history/js/custom/forms.js
@@ -5,37 +5,10 @@ jQuery(document).ready(function($){
   const observer = new MutationObserver(function(mutations_list) {
   	mutations_list.forEach(function(mutation) {
   		mutation.addedNodes.forEach(function(added_node) {
-  			/* if(added_node.id == 'child') {
-  				console.log('#child has been added');
-  				observer.disconnect();
-  			}
-        */
         console.log(added_node);
-        /* if (added_node:contains("Failed").length > 0){
-          console.log('failed');
-        }
-        // failing with SyntaxError: missing ) after condition
-        */
-        /*
-        if ( $(added_node:contains('Failed')).length > 0 ){
-          console.log('failed');
-        }
-        // failing with Uncaught SyntaxError: missing ) after argument list
-        */
-        /*
-        if (added_node.li.outerText:contains('Failed').length > 0 ){
-          console.log('failed');
-        }
-        // failing with Uncaught SyntaxError: missing ) after condition
-        */
-        /*
-        if (added_node.outerText == 'Banquet-Video-1992.mp4 - Failed to move uploaded file.'){
-          console.log('failed');
-        }
-        // works but too specific
-        */
         if (added_node.outerText.indexOf('Failed') >= 0 ){
           console.log('failed');
+          $(added_node).append("<p> Unfortunately, this form can't accept individual files over 256 MB, but we would love to receive this file. <strong>Please email us</strong> at <a href=mailto:'history@scvanguard.org'>history@scvanguard.org</a>, and we can arrange another way for you to upload it.</p>");
         }
         // works but may be brittle if there is additional nesting in the added element
   		});
