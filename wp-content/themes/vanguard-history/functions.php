233695ae280b248a4c6d137bcd4a8099e45dd514
Mike Eng <mike.eng@adhocteam.us>
2022-07-12 11:58:46 -0400

    renamed test.js to forms.js

--- wp-content/themes/vanguard-history/functions.php
+++ wp-content/themes/vanguard-history/functions.php
@@ -148,7 +148,7 @@ function vanguard_history_scripts() {
 
 	wp_enqueue_script( 'vanguard-history-navigation', get_template_directory_uri() . '/js/custom/navigation.js', array(), false, true );
 
-	wp_enqueue_script( 'vanguard-history-test', get_template_directory_uri() . '/js/custom/test.js', array('jquery'), false, true );
+	wp_enqueue_script( 'vanguard-history-test', get_template_directory_uri() . '/js/custom/forms.js', array('jquery'), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
