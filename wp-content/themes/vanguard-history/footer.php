41de2d47c5208d21ace18ec8d321011a24906540
Mike Eng <mike.eng@adhocteam.us>
2022-12-05 11:45:33 -0500

    excluding footer menus from upload material page

--- wp-content/themes/vanguard-history/footer.php
+++ wp-content/themes/vanguard-history/footer.php
@@ -8,25 +8,30 @@
  *
  * @package Vanguard_History
  */
- wp_nav_menu(
-   array(
-     'theme_location' => 'footer-1',
-     'menu_id'        => 'footer-1-menu',
-   )
- );
-?>
-<div id="footer-menu-heading-container">
- <h2 class="footer-menu" id="other-vmapa-sites">Other VMAPA Sites</h2>
-</div>
-<?php
- wp_nav_menu(
-   array(
-     'theme_location' => 'footer-2',
-     'menu_id'        => 'footer-2-menu',
-   )
- );
 
+// Footer menus
+ // Only show on pages / posts other than upload material - for building content before MVP launch
+ if(! (is_page(12))){
+   wp_nav_menu(
+     array(
+       'theme_location' => 'footer-1',
+       'menu_id'        => 'footer-1-menu',
+     )
+   );
+  ?>
+  <div id="footer-menu-heading-container">
+   <h2 class="footer-menu" id="other-vmapa-sites">Other VMAPA Sites</h2>
+  </div>
+  <?php
+   wp_nav_menu(
+     array(
+       'theme_location' => 'footer-2',
+       'menu_id'        => 'footer-2-menu',
+     )
+   );
+  } // end if
 ?>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
