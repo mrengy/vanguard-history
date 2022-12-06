92cd460936d3597999416f09714ffdf2237b9f7f
Mike Eng <mike.eng@adhocteam.us>
2022-12-05 23:43:17 -0500

    showing first show piece but not successfully looping through all

--- wp-content/themes/vanguard-history/template-parts/content.php
+++ wp-content/themes/vanguard-history/template-parts/content.php
@@ -48,34 +48,101 @@
 			if(has_excerpt()){
 				the_excerpt();
 			}
-			
+
 		}
 	?>
 
 	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vanguard-history' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		<section id="story">
+			<?php
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vanguard-history' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vanguard-history' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vanguard-history' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+		</section><!--story-->
+		<section id="show-info">
+			<h2 id="repertoire">
+				Repertoire
+			</h2>
+			<div id="show-title">
+				<?php the_field('show_title');?>
+			</div>
+				<?php
+					$show_piece_counter = 0;
+					if(have_rows('show_pieces')){
+
+					$show_piece_fields = get_field_object('show_pieces');
+					/*
+					$non_empty_show_piece_fields = array_filter($show_piece_fields['value'], function($x) { return ($x !== ""); });
+					$count_non_empty_show_piece_fields = count($non_empty_show_piece_fields);
+					echo ($count_non_empty_show_piece_fields);
+
+					do_action( 'qm/debug', $non_empty_show_piece_fields );
+					*/
+					//do_action( 'qm/debug', $show_piece_fields );
+
+				?>
+				<dl id="show-pieces">
+					<?php
+						while(have_rows('show_pieces')){ the_row();
+							$show_piece_counter ++;
+
+							/*
+							$this_row_debug = get_row();
+							do_action( 'qm/debug', $this_row_debug );
+							*/
+
+							if(have_rows('show_piece_'.$show_piece_counter)){
+								while(have_rows('show_piece_'.$show_piece_counter)){ the_row();
+
+								$inner_row_debug = get_row();
+								do_action( 'qm/debug', $inner_row_debug );
+								do_action( 'qm/debug', $show_piece_counter );
+					?>
+								<dt> <?php the_sub_field('show_piece_title'); ?> </dt>
+								<dd> <?php the_sub_field('show_piece_composer'); ?> </dd>
+					<?php
+								} //while(have_rows('show_pieces')): the_row();
+							} //if(have_rows('show_pieces')):
+
+					?>
+					<?php
+						} //while(have_rows)('show_pieces')): the_row():
+					?>
+				</dl>
+			<?php
+				} //have_rows('show_pieces')
+			?>
+			<div id="final-score-info">
+				<h2 id="final-score-heading">
+					Final Score
+				</h2>
+				<div id="final-score">
+
+				</div>
+				<div id="final-placement">
+					()
+				</div>
+			</div>
+		</section><!-- show-info-->
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer ui container">
