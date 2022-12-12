4495821cd9178c5a2a8439ddba1fdaee9dc1afe6
Mike Eng <mike.eng@adhocteam.us>
2022-12-11 23:36:56 -0500

    only showing show all media button if there is more media

--- wp-content/themes/vanguard-history/single-year_story.php
+++ wp-content/themes/vanguard-history/single-year_story.php
@@ -18,72 +18,80 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-					//get taxonomies from the post
-					$this_ensemble = wp_get_post_terms(get_the_ID(),'ensemble')[0]->slug;
-					$this_year = wp_get_post_terms(get_the_ID(),'vhs_year')[0]->slug;
+			//get taxonomies from the post
+			$this_ensemble = wp_get_post_terms(get_the_ID(),'ensemble')[0]->slug;
+			$this_year = wp_get_post_terms(get_the_ID(),'vhs_year')[0]->slug;
 
-					// query media
-					$media_query_args = array(
-						'post_type'   => 'attachment',
-						'post_status' => 'any',
+			// how many media thumbnails to show at first
+			$thumbnails_to_show = 6;
 
-						'tax_query' => array(
-								'relation' => 'AND',
-								array(
-									'taxonomy' => 'ensemble',
-									'field' => 'slug',
-									'terms' => $this_ensemble,
-								),
-								array(
-									'taxonomy' => 'vhs_year',
-									'field' => 'slug',
-									'terms' => $this_year,
-								),
-								array(
-									'taxonomy' => 'media_visibility',
-									'field' => 'slug',
-									'terms' => 'published',
-								),
-						),
+			// query media
+			$media_query_args = array(
+				'post_type'   => 'attachment',
+				'post_status' => 'any',
 
-						'posts_per_page' => 6,
-					);
-					$media_query = new WP_Query ($media_query_args);
-
-					$thumbnails = array();
-
-					if ( $media_query->have_posts() ) : while ( $media_query->have_posts() ) : $media_query->the_post();
-							// store thumbnails in array
-							$thumbnails[] = wp_get_attachment_link( get_the_ID(), 'thumbnail', true );
-
-						endwhile;
-						//only show heading if there are items to show
-						echo('<h2>Media</h2>');
-					endif; // end of media loop
-
-					// Be kind; rewind
-					wp_reset_postdata();
-
-					//display media
-					echo('<div id="media-container" class="ui grid" data-year="'.$this_year.'" data-ensemble="'.$this_ensemble.'">');
-
-						foreach($thumbnails as $thumbnail){
-							echo($thumbnail);
-						}
-
-					echo('</div>') // closing tag for media-container
-					?>
-
-					<button class="show-hide" id="show-all-media"><span class="button-action">Show</span> all media</button>
-
-					<h2 class="footer-menu" id="help-us-tell-more-stories">Help us tell more stories</h2>
-					<?php
-					wp_nav_menu(
+				'tax_query' => array(
+						'relation' => 'AND',
 						array(
-							'theme_location' => 'footer-calls-to-action',
-							'menu_id'        => 'footer-calls-to-action',
-						)
-					);
+							'taxonomy' => 'ensemble',
+							'field' => 'slug',
+							'terms' => $this_ensemble,
+						),
+						array(
+							'taxonomy' => 'vhs_year',
+							'field' => 'slug',
+							'terms' => $this_year,
+						),
+						array(
+							'taxonomy' => 'media_visibility',
+							'field' => 'slug',
+							'terms' => 'published',
+						),
+				),
+
+				'posts_per_page' => $thumbnails_to_show,
+			);
+			$media_query = new WP_Query ($media_query_args);
+
+			$thumbnails = array();
+
+			if ( $media_query->have_posts() ) : while ( $media_query->have_posts() ) : $media_query->the_post();
+					// store thumbnails in array
+					$thumbnails[] = wp_get_attachment_link( get_the_ID(), 'thumbnail', true );
+
+				endwhile;
+				//only show heading if there are items to show
+				echo('<h2>Media</h2>');
+			endif; // end of media loop
+
+			// Be kind; rewind
+			wp_reset_postdata();
+
+			//display media
+			echo('<div id="media-container" class="ui grid" data-year="'.$this_year.'" data-ensemble="'.$this_ensemble.'">');
+
+				foreach($thumbnails as $thumbnail){
+					echo($thumbnail);
+				}
+
+			echo('</div>'); // closing tag for media-container
+
+			$num_thumbnails_returned = count($thumbnails);
+			if($num_thumbnails_returned >= $thumbnails_to_show){
+				?>
+					<button class="show-hide" id="show-all-media"><span class="button-action">Show</span> all media</button>
+				<?php
+			}
+			?>
+
+			<h2 class="footer-menu" id="help-us-tell-more-stories">Help us tell more stories</h2>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'footer-calls-to-action',
+					'menu_id'        => 'footer-calls-to-action',
+				)
+			);
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
