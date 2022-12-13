c9b5dd969a9b5660338cb1bba65617313f5c7782
shekharkhedekar <shekhar.khedekar@gmail.com>
2022-12-12 17:08:18 -0800

    Add header styles

--- wp-content/themes/vanguard-history/header.php
+++ wp-content/themes/vanguard-history/header.php
@@ -12,46 +12,57 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'vanguard-history' ); ?></a>
-	<?php
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text"
+            href="#primary"><?php esc_html_e( 'Skip to content', 'vanguard-history' ); ?></a>
+        <?php
 		$excluded_pages = array('upload-material', 'upload-confirmation');
 		if (! is_page($excluded_pages)){ ?>
-		<header id="masthead" class="site-header ui container">
-			<div class="site-branding">
-				<?php
+        <header id="masthead" class="site-header ui">
+            <div class="site-logo">
+                <img src="wp-content/themes/vanguard-history/img/Star.svg" />
+            </div>
+            <div class=" site-branding">
+                <?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
 					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php
 					$vanguard_history_description = get_bloginfo( 'description', 'display' );
 					if ( $vanguard_history_description || is_customize_preview() ) :
 						?>
-						<p class="site-description"><?php echo $vanguard_history_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-					<?php
+                <p class="site-description">
+                    <?php echo $vanguard_history_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                </p>
+                <?php
 					endif;
 				else :
 					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
 				endif;
 				?>
-			</div><!-- .site-branding -->
+            </div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'vanguard-history' ); ?></button>
-				<?php
+            <nav id="site-navigation" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                    <!-- <?php esc_html_e( 'Primary Menu', 'vanguard-history' ); ?> -->
+                </button>
+                <?php
 				wp_nav_menu(
 					array(
 						'theme_location' => 'menu-1',
@@ -59,6 +70,6 @@
 					)
 				);
 				?>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
-	<?php } ?>
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
+        <?php } ?>
