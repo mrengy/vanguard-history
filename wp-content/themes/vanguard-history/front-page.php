6ad19c84cfc70dfc3308985260b82481e7b09fcc
Mike Eng <mike.eng@adhocteam.us>
2023-01-12 15:57:19 -0500

    initial query of recent media

--- wp-content/themes/vanguard-history/front-page.php
+++ wp-content/themes/vanguard-history/front-page.php
@@ -38,9 +38,72 @@ get_header();
 				Recent Uploads
 			</h1>
 			<?php
-			// query media
+				// query media
+				// how many media thumbnails to show at first
+				$thumbnails_to_show = 6;
 
-			?>
+				// query media
+				$media_query_args = array(
+					'post_type'   => 'attachment',
+					'post_status' => 'any',
+					'order' => 'DESC',
+					'orderby' => 'date',
+
+					'tax_query' => array(
+							'relation' => 'AND',
+							array(
+								'taxonomy' => 'media_visibility',
+								'field' => 'slug',
+								'terms' => 'published',
+							),
+					),
+
+					'posts_per_page' => $thumbnails_to_show,
+				);
+				$media_query = new WP_Query ($media_query_args);
+
+				$thumbnails = array();
+
+				if ( $media_query->have_posts() ) : while ( $media_query->have_posts() ) : $media_query->the_post();
+						// store thumbnails in array
+						$thumbnails[] = wp_get_attachment_link( get_the_ID(), 'thumbnail', true );
+
+					endwhile;
+
+				endif; // end of media loop
+
+				$thumbnails_count = count($thumbnails);
+
+				// Be kind; rewind
+				wp_reset_postdata();
+
+				if ($thumbnails_count>0) { ?>
+			    <div class="year-media year-section media-grid">
+			        <h2 class="entry-heading">Media</h2>
+			        <div class="container">
+			            <div id="media-container" class="all-media-grid">
+			                <?php
+												foreach($thumbnails as $thumbnail){
+													echo($thumbnail);
+												}
+											?>
+          				</div>
+
+							<?php
+							//how many thumbnails did we load?
+							$num_thumbnails_returned = $thumbnails_count;
+
+							// only display the "show all media" button if there is more media
+							if($num_thumbnails_returned >= $thumbnails_to_show){?>
+			            <div class="button-container">
+			                <button class="show-hide button button-primary" id="show-more-media">
+			                    Show <span class="button-action">more</span> media
+			                </button>
+			            </div>
+	            <?php } ?>
+	        		</div>
+	    		</div>
+	    <?php } ?>
 		</section>
 	</main><!-- #main -->
 
