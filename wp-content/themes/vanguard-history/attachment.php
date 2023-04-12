2eb737aaa329e613514ee3134eb3e87c80390a8b
Mike Eng <mike.eng@adhocteam.us>
2023-04-12 17:02:53 -0400

    back link, with conditional logic

--- wp-content/themes/vanguard-history/attachment.php
+++ wp-content/themes/vanguard-history/attachment.php
@@ -14,63 +14,79 @@ get_header();
 <main id="primary" class="site-main content-section content-secondary">
 
 	<?php
-	while (have_posts()) :
-		the_post();
+		$previous = $_SERVER['HTTP_REFERER'];
+		$site_url = get_site_url();
 
-		$post->{'media_visibility'} = get_the_terms(get_the_ID(), 'media_visibility');
-		$media_visibility = $post->{'media_visibility'}[0]->{'slug'};
-
-		//only display attachment if the "visibility" is set to "published"
-		if($media_visibility == 'published'){
-			get_template_part('template-parts/content', get_post_type());
-		} else {
-			show_404_in_page();
+		// if the previous page in browser history is within the site, use that previous page. Otherwise, use the media page 
+		if( str_contains($previous, $site_url) ){
+			$back_link = $previous;
+		} else{
+			$back_link = get_permalink( get_page_by_path('media'));
 		}
 
-	/*
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'vanguard-history' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'vanguard-history' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-			*/
-	//currently can go back to a parent page but want to navigate through in order shown on media page
-	/*
-			previous_post_link();
-			next_post_link();
-			*/
-	/*
-			$vhs_previous_link = get_previous_image_link('large');
-			$vhs_next_link = get_next_image_link('large');
-			?>
+		echo <<<END
+			<div id="back-link">
+				<a href="$back_link">« Back</a>
+			</div>
+		END;
 
-    <nav id="previous-next-nav">
-        <?php
-			// these are not displaying, but it seems to be showing up somehow
-			if (!empty($vhs_previous_link)){
-					echo("<div id='previous'>
-						Previous: $vhs_previous_link
-					</div>");
+		while (have_posts()) :
+			the_post();
+
+			$post->{'media_visibility'} = get_the_terms(get_the_ID(), 'media_visibility');
+			$media_visibility = $post->{'media_visibility'}[0]->{'slug'};
+
+			//only display attachment if the "visibility" is set to "published"
+			if($media_visibility == 'published'){
+				get_template_part('template-parts/content', get_post_type());
+			} else {
+				show_404_in_page();
 			}
 
-			if (!empty($vhs_next_link)){
-					echo("<div id='next'>
-						Next: $vhs_next_link
-					</div>");
-			}
-			?>
-    </nav>
+		/*
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'vanguard-history' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'vanguard-history' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
+				*/
+		//currently can go back to a parent page but want to navigate through in order shown on media page
+		/*
+				previous_post_link();
+				next_post_link();
+				*/
+		/*
+				$vhs_previous_link = get_previous_image_link('large');
+				$vhs_next_link = get_next_image_link('large');
+				?>
 
-    <?php
-			*/
-	// If comments are open or we have at least one comment, load up the comment template.
-	/*
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-			*/
-	endwhile; // End of the loop.
+		<nav id="previous-next-nav">
+			<?php
+				// these are not displaying, but it seems to be showing up somehow
+				if (!empty($vhs_previous_link)){
+						echo("<div id='previous'>
+							Previous: $vhs_previous_link
+						</div>");
+				}
+
+				if (!empty($vhs_next_link)){
+						echo("<div id='next'>
+							Next: $vhs_next_link
+						</div>");
+				}
+				?>
+		</nav>
+
+		<?php
+				*/
+		// If comments are open or we have at least one comment, load up the comment template.
+		/*
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+				*/
+		endwhile; // End of the loop.
 	?>
 
 </main><!-- #main -->
